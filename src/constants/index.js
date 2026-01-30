2df90d35460560c8bd5a19c5438884ea9ac2c52e
michael-bardouille <michael.bardouille@gmail.com>
2026-01-30 12:09:24 -0500

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "iPhone" },
+  { label: "Watch" },
+  { label: "Vision" },
+  { label: "AirPods" },
+];
+export { navLinks };
